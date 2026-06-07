eda1ef702e64cb7fdbf9f07df2fe9602b131ffd2
monprevm <monuttipreviderem@gmail.com>
2026-06-07 19:38:58 -0300

    MapaNivel3

---
+++ public/assets/tilemap2/texture.tsx
@@ -0,0 +1,84 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="texture" tilewidth="24" tileheight="24" tilecount="924" columns="33">
+ <image source="../tp3-dt2-phaser3/public/assets/texture.png" width="800" height="680"/>
+ <tile id="825">
+  <objectgroup>
+   <object id="1" type="Plataformas" x="0" y="0" width="24" height="24"/>
+  </objectgroup>
+ </tile>
+ <tile id="826">
+  <objectgroup>
+   <object id="1" type="Plataformas" x="0" y="0" width="24" height="24"/>
+  </objectgroup>
+ </tile>
+ <tile id="827">
+  <objectgroup>
+   <object id="1" type="Plataformas" x="0" y="0" width="24" height="24"/>
+  </objectgroup>
+ </tile>
+ <tile id="828">
+  <objectgroup>
+   <object id="1" type="Plataformas" x="0" y="0" width="24" height="24"/>
+  </objectgroup>
+ </tile>
+ <tile id="829">
+  <objectgroup>
+   <object id="1" type="Plataformas" x="0" y="0" width="24" height="24"/>
+  </objectgroup>
+ </tile>
+ <tile id="830">
+  <objectgroup>
+   <object id="1" type="Plataformas" x="0" y="0" width="24" height="24"/>
+  </objectgroup>
+ </tile>
+ <tile id="831">
+  <objectgroup>
+   <object id="1" type="Plataformas" x="0" y="0" width="24" height="24"/>
+  </objectgroup>
+ </tile>
+ <tile id="832">
+  <objectgroup>
+   <object id="1" type="Plataformas" x="0" y="0" width="24" height="24"/>
+  </objectgroup>
+ </tile>
+ <tile id="833">
+  <objectgroup>
+   <object id="1" type="Plataformas" x="0" y="0" width="24" height="24"/>
+  </objectgroup>
+ </tile>
+ <tile id="834">
+  <objectgroup>
+   <object id="1" type="Plataformas" x="0" y="0" width="24" height="24"/>
+  </objectgroup>
+ </tile>
+ <tile id="835">
+  <objectgroup>
+   <object id="1" type="Plataformas" x="0" y="0" width="24" height="24"/>
+  </objectgroup>
+ </tile>
+ <tile id="836">
+  <objectgroup>
+   <object id="1" type="Plataformas" x="0" y="0" width="24" height="24"/>
+  </objectgroup>
+ </tile>
+ <tile id="837">
+  <objectgroup>
+   <object id="1" type="Plataformas" x="0" y="0" width="24" height="24"/>
+  </objectgroup>
+ </tile>
+ <tile id="838">
+  <objectgroup>
+   <object id="1" type="Plataformas" x="0" y="0" width="24" height="24"/>
+  </objectgroup>
+ </tile>
+ <tile id="839">
+  <objectgroup>
+   <object id="1" type="Plataformas" x="0" y="0" width="24" height="24"/>
+  </objectgroup>
+ </tile>
+ <tile id="840">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="Plataformas" x="0" y="0" width="24" height="24"/>
+  </objectgroup>
+ </tile>
+</tileset>
